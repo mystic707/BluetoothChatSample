32de163f504a6f0b81a4b35901bcafb7c197c8af
jschoi <jschoi@com2us.com>
2023-05-16 19:42:19 +0900

    - remove useless annotation

--- app/src/main/java/com/mysticagit/bluetooth/sample/SampleDataManager.kt
+++ app/src/main/java/com/mysticagit/bluetooth/sample/SampleDataManager.kt
@@ -7,13 +7,7 @@ import java.util.*
 object SampleDataManager {
 
     const val logTag = "mysticagit"
-//    val myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")  // SerialPortServiceClass_UUID (포럼들에서 연결되었다는 값)
-//    val myUUID: UUID = UUID.fromString("00000000-0000-1000-8000-00805F9B34FB")
-//    val myUUID: UUID = UUID.fromString("0000112f-0000-1000-8000-00805f9b34fb")    // connectable
-//    val myUUID: UUID = UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66")    // 유투브 샘플 영상에서 사용한 UUID
     val myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
-//val myUUID: UUID = UUID.fromString("26b7d1da-08c7-4505-a6d1-2459987e5e2d")
-
 
     data class PairableDeviceInfo(
         var deviceName: String?,
